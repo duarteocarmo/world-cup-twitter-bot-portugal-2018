092f597cf02a1b48892aa0fea7f2acbcc5808d7a
Duarte OC <duarteocarmo@gmail.com>
2017-12-28 14:55:01 +0000

    removed config for privacy

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'LKW8xwdgIB7rMmLMqSSGxY3gM',
-    consumer_secret:      'rnu8kKbE78DFhV2tNbB56yiIlrpWJ98C7zp50NpDcXf6G1IrUB',
-    access_token:         '944280016929611778-60fMuyZr7DheWk3aZrS5HW8F5JDB36N',
-    access_token_secret:  '7tN7gR9AtvB8oqecwsCh7y8yemOkHhYp4CzN3zUlaIYKb',
-}
